ac7183518f37659feb5c29eeafca71ac83e1d34d
suhut <suhut.wadiyo@gmail.com>
2024-09-07 12:31:30 +0700

    first commit

---
+++ Coba.CodeGen.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Coba.CodeGen.ConsoleApp.Model; 
+
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("  Wired Brain Coffee - Person Manager  ");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine();
+
+var person = new Person
+{
+    FirstName = "Thomas",
+    LastName = "Huber"
+};
+
+var personAsString = person.ToString();
+
+Console.WriteLine(personAsString);
+
+Console.ReadLine();
---
+++ Coba.CodeGen.Generators/ToStringGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Coba.CodeGen.Generators;
+
+[Generator]
+public sealed class ToStringGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+
+    }
+}
